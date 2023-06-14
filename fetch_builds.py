465a8545021090617eea1f9823242bbe4f71e2ca
Denis Rystsov <denis@vectorized.io>
2023-06-14 14:06:01 -0700

    add fetch_build paging

--- fetch_builds.py
+++ fetch_builds.py
@@ -3,6 +3,9 @@ import re
 import json
 import os.path
 import os
+import time
+
+r = re.compile("summary-ducktape-build-([a-z]+)-clang-([a-z0-9]+)-0")
 
 def load_json(name, folder="builds"):
     with open(f"{folder}/{name}.json", "r") as f:
@@ -25,40 +28,55 @@ else:
         "last-id": -1
     }
 
+print("Fetching a list of recent ci-runs")
+
 conn = psycopg2.connect(f"host={host} dbname={database} user={user} password={password}")
 cur = conn.cursor()
-cur.execute(f"SELECT id, ts, name, data, meta FROM test_results WHERE id > {manifest['last-id']} AND name LIKE 'summary-%' ORDER BY ts ASC")
-records = cur.fetchall()
 
-r = re.compile("summary-ducktape-build-([a-z]+)-clang-([a-z0-9]+)-0")
+
+cur.execute(f"SELECT count(*) FROM test_results WHERE id > {manifest['last-id']} AND name LIKE 'summary-%'")
+count = cur.fetchone()[0]
+
+if count > 0:
+    print(f"Fetching {count} ci-runs")
+else:
+    print("No new ci runs were found")
 
 fetched = 0
+while True:
+    started = time.time()
+    cur.execute(f"SELECT id, ts, name, data, meta FROM test_results WHERE id > {manifest['last-id']} AND name LIKE 'summary-%' ORDER BY id ASC LIMIT 100")
+    records = cur.fetchall()
 
-for record in records:
-    fetched += 1
+    if len(records) == 0:
+        break
 
-    m = r.match(record[2])
-    if not m:
-        assert False
+    for record in records:
+        fetched += 1
+
+        m = r.match(record[2])
+        if not m:
+            assert False
     
-    build = {
-        "id": record[0],
-        "day": record[1].strftime("%Y-%m-%d"),
-        "ts": record[1].timestamp(),
-        "build": m.group(1),
-        "arch": m.group(2),
-        "name": record[2],
-        "data": record[3],
-        "meta": record[4]
-    }
+        build = {
+            "id": record[0],
+            "day": record[1].strftime("%Y-%m-%d"),
+            "ts": record[1].timestamp(),
+            "build": m.group(1),
+            "arch": m.group(2),
+            "name": record[2],
+            "data": record[3],
+            "meta": record[4]
+        }
 
-    manifest["builds"].append(record[0])
-    manifest["last-id"] = record[0]
+        manifest["builds"].append(record[0])
+        manifest["last-id"] = record[0]
 
-    with open("builds/manifest.json", "w") as m:
-        json.dump(manifest, m)
+        with open("builds/manifest.json", "w") as m:
+            json.dump(manifest, m)
     
-    with open(f"builds/{record[0]}.json", "w") as b:
-        json.dump(build, b)
-
-print(f"fetched {fetched} builds")
+        with open(f"builds/{record[0]}.json", "w") as b:
+            json.dump(build, b)
+    
+    ended = time.time()
+    print(f"\tfetched {fetched} / {count} in {int(ended - started)}s")
