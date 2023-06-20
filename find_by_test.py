4f7f18b5d0b9fab70143225ce57f7c5f76bbf658
Denis Rystsov <denis@vectorized.io>
2023-06-20 15:57:55 -0700

    add find_by_test script

---
+++ find_by_test.py
@@ -0,0 +1,11 @@
+import json
+import sys
+from index import load_json
+
+manifest = load_json("data/failures/manifest")
+
+for id in manifest["failures"]:
+    failure = load_json(f"data/failures/{id}")
+
+    if sys.argv[1] in failure["test_id"]:
+        print(f"{id} {failure['test_id']}")
