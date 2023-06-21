7c6abeca8a2d132d265cb0f95aa85a8c44fad816
Travis Downs <travis.downs@gmail.com>
2023-06-21 15:27:34 -0700

    Allow a --since argument on view_new
    This restricts the output to failures which have
    occurred within the last PERIOD time, to support the existing
    triage flow.

---
+++ shared.py
@@ -0,0 +1,24 @@
+# utilities shared among the various tools
+
+import re
+from datetime import timedelta
+
+regex = re.compile(r'^((?P<days>[\.\d]+?)d)?((?P<hours>[\.\d]+?)h)?((?P<minutes>[\.\d]+?)m)?((?P<seconds>[\.\d]+?)s)?$')
+
+def parse_time(time_str: str):
+    """
+    Parse a time string e.g. (2h13m) into a timedelta object.
+
+    Modified from virhilo's answer at https://stackoverflow.com/a/4628148/851699
+
+    :param time_str: A string identifying a duration.  (eg. 2h13m)
+    :return datetime.timedelta: A datetime.timedelta object
+
+    CC BY-SA 4.0
+    https://creativecommons.org/licenses/by-sa/4.0/
+    """
+
+    parts = regex.match(time_str)
+    assert parts is not None, "Could not parse any time information from '{}'.  Examples of valid strings: '8h', '2d8h5m20s', '2m4s'".format(time_str)
+    time_params = {name: float(param) for name, param in parts.groupdict().items() if param}
+    return timedelta(**time_params)
