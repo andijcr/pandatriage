9712a2019a15ce1a8bf06205f9c51ec9cc25fd15
Denis Rystsov <denis@vectorized.io>
2023-06-22 09:33:05 -0700

    fix find_by_issue script

--- find_by_issue.py
+++ find_by_issue.py
@@ -1,14 +1,10 @@
-import json
 import sys
+from index import load_json
 
-def load_json(name, folder="failures"):
-    with open(f"{folder}/{name}.json", "r") as f:
-        return json.load(f)
-
-manifest = load_json("manifest")
+manifest = load_json("data/failures/manifest")
 
 for id in manifest["failures"]:
-    failure = load_json(id)
+    failure = load_json(f"data/failures/{id}")
 
     for issue in failure["issues"]:
         if int(sys.argv[1]) == issue["number"]:
